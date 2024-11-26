81a5e1f1e6b50dc7d91333f03a3cf6dd016bc395
JuanseUnU <sebasgalviss1415@gmail.com>
2024-11-26 15:31:41 -0500

    Se termina de configurar todo

--- js/app.js
+++ js/app.js
@@ -17,3 +17,52 @@ document.addEventListener("DOMContentLoaded", async () => {
         `).join("");
 });
 
+// Crear la vista de detalles para cada producto al dar click en el boton ver más
+window.viewProduct = async (id) => {
+    const product = await getProductById(id);
+    const productDetails = `
+      <div class="col">
+        <img class="img-fluid" src="${product.imgUrl}">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <p>Precio: ${new Intl.NumberFormat('en-ES', { style: 'currency', currency: 'USD' }).format(product.price)}</p>
+        <button class="btn btn-warning" onclick="enableEdit(${product.id})">Editar</button>
+        <button class="btn btn-danger" onclick="deleteProduct(${product.id})">Eliminar</button>
+      </div>
+      `;
+    document.getElementById('product-list').innerHTML = productDetails;
+  };
+  
+  // Habilitamos el formulario para editar cada uno de los productos
+  window.enableEdit = async (id) => {
+    const product = await getProductById(id);
+    const editForm = `
+      <div class="row gap-3">
+        <input type="text" id="name" value="${product.name}">
+        <textarea id="description">${product.description}</textarea>
+        <input type="number" id="price" value="${product.price}">
+        <input type="text" id="imgUrl" value="${product.imgUrl}">
+        <button class="btn btn-success" onclick="saveEdit(${id})">Guardar</button>
+      </div>
+      `;
+    document.getElementById('product-list').innerHTML = editForm;
+  };
+  
+  // Guardamos la nueva información en nuestra base de datos
+  window.saveEdit = async (id) => {
+    const updatedProduct = {
+      name: document.getElementById('name').value,
+      description: document.getElementById('description').value,
+      price: parseFloat(document.getElementById('price').value),
+      imgUrl: document.getElementById('imgUrl').value
+    };
+    await updateProduct(id, updatedProduct);
+    location.reload(); // Recarga la página para ver los cambios
+  };
+  
+  // Función para borrar el producto seleccionado
+  window.deleteProduct = async (id) => {
+    await deleteProduct(id);
+    location.reload(); // Recarga la página para ver los cambios
+  };
+  
